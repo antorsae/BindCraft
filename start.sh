f469827c053b992ed96dd79971e6e387e1d265a9
Andres Torrubia <andres@medbravo.com>
2024-10-17 21:23:20 +0200

    EGFR task

---
+++ start.sh
@@ -0,0 +1,2 @@
+conda activate BindCraft
+python -u ./bindcraft.py --settings './settings_target/EGFR.json' --filters './settings_filters/default_filters.json' --advanced './settings_advanced/4stage_multimer.json'
