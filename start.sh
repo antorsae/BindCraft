7bd81217752fa5c407ae0f252d9c1f483818daa1
Andres Torrubia <andres@medbravo.com>
2024-10-19 21:42:05 +0200

    update

--- start.sh
+++ start.sh
@@ -1,1 +1,4 @@
+chmod +x functions/DAlphaBall.gcc
+chmod +x functions/dssp
 python -u ./bindcraft.py --settings './settings_target/EGFR-martin.pacesa-EGFR_l138_s90285_mpnn2.json' --filters './settings_filters/default_filters.json' --advanced './settings_advanced/4stage_multimer.json'
+
