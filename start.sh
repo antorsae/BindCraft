28f55d67fc8e0a862fa72b8e5eaa45d1341b4cbb
Andres Torrubia <andres@medbravo.com>
2024-10-19 21:48:48 +0200

    redux

--- start.sh
+++ start.sh
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-conda activate BindCraft
-python -u ./bindcraft.py --settings './settings_target/EGFR.json' --filters './settings_filters/default_filters.json' --advanced './settings_advanced/4stage_multimer.json'
-=======
 chmod +x functions/DAlphaBall.gcc
 chmod +x functions/dssp
-python -u ./bindcraft.py --settings './settings_target/EGFR-martin.pacesa-EGFR_l138_s90285_mpnn2.json' --filters './settings_filters/default_filters.json' --advanced './settings_advanced/4stage_multimer.json'
-
->>>>>>> 7bd8121 (update)
+python -u ./bindcraft.py --settings './settings_target/EGFR.json' --filters './settings_filters/default_filters.json' --advanced './settings_advanced/4stage_multimer.json'
