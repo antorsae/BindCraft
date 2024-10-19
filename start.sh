53ce18d19c6d66b23d3670f9c41c787d640c5fd3
root <andres@>
2024-10-19 20:53:46 +0000

    v1: only target 356, 440-441

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 chmod +x functions/DAlphaBall.gcc
 chmod +x functions/dssp
-python -u ./bindcraft.py --settings './settings_target/EGFR.json' --filters './settings_filters/default_filters.json' --advanced './settings_advanced/4stage_multimer.json'
+python -u ./bindcraft.py --settings './settings_target/EGFR-v1.json' --filters './settings_filters/default_filters.json' --advanced './settings_advanced/4stage_multimer.json'
