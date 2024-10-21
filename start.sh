d69cd563bf3e4aa691659c3b7399f7e32740f1b6
Andres Torrubia <andres@medbravo.com>
2024-10-21 23:35:18 +0200

    v5

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 chmod +x functions/DAlphaBall.gcc
 chmod +x functions/dssp
-python -u ./bindcraft.py --settings './settings_target/EGFR-v4.json' --filters './settings_filters/EGFR_filters.json' --advanced './settings_advanced/4stage_multimer.json'
+python -u ./bindcraft.py --settings './settings_target/EGFR-v5.json' --filters './settings_filters/EGFR_filters.json' --advanced './settings_advanced/4stage_multimer.json'
