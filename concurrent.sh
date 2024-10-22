5b880801023a7c5e6ee8627d797b16d7f88f0954
Andres Torrubia <andres@runpot>
2024-10-22 09:47:19 +0000

    concurrent v7

--- concurrent.sh
+++ concurrent.sh
@@ -3,7 +3,7 @@ chmod +x functions/dssp
 for i in {0..5}
 do
    CUDA_VISIBLE_DEVICES=$i python -u ./bindcraft.py \
-     --settings './settings_target/EGFR-v6.json' \
+     --settings './settings_target/EGFR-v7.json' \
      --filters './settings_filters/default_filters.json' \
      --advanced './settings_advanced/4stage_multimer.json' \
      &> out${i}.txt &
