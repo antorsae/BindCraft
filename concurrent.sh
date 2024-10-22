f82420fbe31aad4974bfa450de318a0dc2bbf5ae
Andres Torrubia <andres@runpot>
2024-10-22 07:57:40 +0000

    redux v6: 307,308, 340, 458

--- concurrent.sh
+++ concurrent.sh
@@ -1,9 +1,9 @@
 chmod +x functions/DAlphaBall.gcc
 chmod +x functions/dssp
-for i in {0..6}
+for i in {0..5}
 do
    CUDA_VISIBLE_DEVICES=$i python -u ./bindcraft.py \
-     --settings './settings_target/PDL1.json' \
+     --settings './settings_target/EGFR-v6.json' \
      --filters './settings_filters/default_filters.json' \
      --advanced './settings_advanced/4stage_multimer.json' \
      &> out${i}.txt &
